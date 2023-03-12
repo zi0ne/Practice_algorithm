72b8884b113265eac1acd1f84508c6dafb9e1882
zi0ne <jiwon0331j@gmail.com>
2023-03-12 12:23:34 +0000

    daily code

---
+++ daily/03_balancedBrackets.js
@@ -0,0 +1,43 @@
+// naive solution
+// const balancedBrackets = function (str) {
+//   const stack = [];
+//   const opener = '(';
+//   const closer = ')';
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === opener) {
+//       stack.push(str[i]);
+//     } else if (str[i] === closer) {
+//       const top = stack.pop();
+//       if (top !== opener) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
+
+const balancedBrackets = function (str) {
+  const stack = [];
+  const opener = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+  };
+  const closer = '}])';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in opener) {
+      stack.push(str[i]);
+    } else if (closer.includes(str[i])) {
+      const top = stack.pop();
+      const pair = opener[top];
+      if (pair !== str[i]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
